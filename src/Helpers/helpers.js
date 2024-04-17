38ff48e9ed02815ea891a5512392da0fba01f238
Jennifer <jenniferpeterson@pursuit.org>
2024-04-17 19:42:08 -0400

    updated the helper functions

--- src/Helpers/helpers.js
+++ src/Helpers/helpers.js
@@ -36,3 +36,19 @@ export const getClimateColor = (climate) => {
       return "";
   }
 };
+
+export const capitalizeFirstLetter = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+export const formatDateToEdit = (dateString) => {
+  const date = new Date(dateString);
+
+  // Extract year, month, and day
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Add leading zero if needed
+  const day = String(date.getDate()).padStart(2, "0"); // Add leading zero if needed
+
+  // Return formatted date string in "yyyy-MM-dd" format
+  return `${year}-${month}-${day}`;
+};
