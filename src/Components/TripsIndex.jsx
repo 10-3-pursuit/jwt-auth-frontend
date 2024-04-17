3abb5592a574bae78e804316ee37f85b1e0bd1ea
Jennifer <jenniferpeterson@pursuit.org>
2024-04-16 22:20:14 -0400

    did some styling on the trips cards

--- src/Components/TripsIndex.jsx
+++ src/Components/TripsIndex.jsx
@@ -2,10 +2,12 @@ import { useOutletContext } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useNavigate, Link, useParams } from "react-router-dom";
 import { formatDate, getClimateColor } from "../Helpers/helpers";
+import { Trash2 } from "lucide-react";
+import { MapPin } from "lucide-react";
+import { MapPinOff } from "lucide-react";
 
 const API = import.meta.env.VITE_BASE_URL;
 const TripsIndex = () => {
-  const { id } = useParams();
   const navigate = useNavigate();
   const { user } = useOutletContext(); // Access user data provided by the Outlet's context
 
@@ -26,10 +28,29 @@ const TripsIndex = () => {
         })
         .catch((error) => console.error("Error fetching user:", error));
     }
-  }, [trips]);
+  }, [user]);
 
   const handleClick = (id) => {
-    navigate(`/entries/${id}`);
+    navigate(`/trips/${id}/entries`);
+  };
+
+  const handleDelete = (tripId) => {
+    const token = localStorage.getItem("token");
+    fetch(`${API}/api/trips/${tripId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Remove the deleted trip from the state
+          setTrips(trips.filter((trip) => trip.id !== tripId));
+        } else {
+          throw new Error("Failed to delete trip");
+        }
+      })
+      .catch((error) => console.error("Error deleting trip:", error));
   };
 
   return (
@@ -61,28 +82,53 @@ const TripsIndex = () => {
               key={id}
               className="border-4 border-gray-950 mb-10 ml-3 rounded-lg"
             >
-              <div className="destination">
+              <div>
                 <h2
                   className={`${getClimateColor(
                     climate
-                  )} text-2xl text-center rounded-sm px-3 mb-1`}
+                  )} text-2xl flex items-center rounded-sm px-3`}
                 >
                   {destination}
+                  <div className="flex ml-auto">
+                    <h3 className="mr-2">
+                      {first_time ? (
+                        <span>
+                          <MapPinOff size={24} />
+                        </span>
+                      ) : (
+                        <MapPin size={24} />
+                      )}
+                    </h3>
+
+                    <Trash2
+                      className="hover:text-white"
+                      size={24}
+                      onClick={() => handleDelete(id)}
+                    />
+                  </div>
                 </h2>
               </div>
-              <div>Start Date:{formatDate(start_date)}</div>
+              <div className="p-2">
+                <div>Start Date:{formatDate(start_date)}</div>
 
-              <p>End Date: {formatDate(end_date)}</p>
-              <p>Budget: ${budget}</p>
-              <p>Spent so far: ${total_cost}</p>
-              <p className={climate}>Climate: {climate}</p>
-              <p># of entries:{entry_count}</p>
-              <button
-                onClick={() => handleClick(id)}
-                className="border-2 border-black-500 mb-2"
-              >
-                Click to see the entries
-              </button>
+                <p>End Date: {formatDate(end_date)}</p>
+                <p>Budget: ${budget}</p>
+                <p>Spent so far: ${total_cost}</p>
+                <p className={climate}>Climate: {climate}</p>
+                <p># of entries:{entry_count}</p>
+                <div className="flex justify-center mt-2 pb-2">
+                  <button
+                    onClick={() => handleClick(id)}
+                    className="bg-green-300  rounded p-1 hover:bg-slate-200 w-1/3 mr-8"
+                  >
+                    Entries
+                  </button>
+
+                  <button className="bg-orange-300 rounded p-1 hover:bg-slate-200 w-1/3">
+                    Edit
+                  </button>
+                </div>
+              </div>
             </div>
           )
         )}
